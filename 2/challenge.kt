ad4281d410f49dd186572acc27be198b2b177084
Andre Alonzo <analonzo@cj.com>
2022-12-21 14:18:20 -0800

    refactor

--- 2/challenge.kt
+++ 2/challenge.kt
@@ -2,7 +2,34 @@ package com.aalonzo.adventofcode2020.two
 
 import java.io.File
 
-fun answer1(): Int {
+
+fun isPart1PasswordValid(password: Password): Boolean {
+    val length = password.password.count { it == password.letter }
+    return (length >= password.min && length <= password.max)
+}
+
+fun answer(passwords: List<Password>, isValid: (Password) -> Boolean): Int {
+    var numValid = 0
+    for (password in passwords) {
+        if (isValid(password)) {
+            numValid++
+        }
+    }
+    return numValid
+}
+
+fun isPart2PasswordValid(password: Password): Boolean {
+    var numLetter = 0
+    if (password.password[password.min - 1] == password.letter) {
+        numLetter++
+    }
+    if (password.password[password.max - 1] == password.letter) {
+        numLetter++
+    }
+    return numLetter == 1
+}
+
+private fun parseInput(): List<Password> {
     val list = File("./2/input.txt").readLines()
     val listArr = list.map { x -> x.split(" ") }
 
@@ -14,55 +41,20 @@ fun answer1(): Int {
         val password = line[2]
         Password(min, max, letter, password)
     }
-    var numValid = 0
-    for (password in passwords) {
-        println(password)
-        val length = password.password.count { it == password.letter }
-        if (length >= password.min && length <= password.max) {
-            numValid++
-        }
-    }
-    return numValid
+    return passwords
 }
 
-fun answer2(): Int {
-
-    val list = File("./2/input.txt").readLines()
-    val listArr = list.map { x -> x.split(" ") }
-
-    val passwords = listArr.map { line ->
-        val minMax = line[0].split("-")
-        val min = minMax[0].toInt()
-        val max = minMax[1].toInt()
-        val letter = line[1].split(":")[0].first()
-        val password = line[2]
-        Password(min, max, letter, password)
-    }
-    var numValid = 0
-    for (password in passwords) {
-        var numLetter = 0
-        if(password.password.get(password.min-1).equals(password.letter)){
-            numLetter++
-        }
-        if(password.password.get(password.max-1).equals(password.letter)){
-            numLetter++
-        }
-        if(numLetter ==1){
-            numValid++
-        }
-    }
-    return numValid
-}
 
 fun main() {
-    val answer1 = answer1()
+    val passwords = parseInput()
+    val answer1 = answer(passwords, ::isPart1PasswordValid)
     println("Answer 1: $answer1")
-//
-    val answer2 = answer2()
+
+    val answer2 = answer(passwords, ::isPart2PasswordValid)
     println("Answer 2: $answer2")
 }
 
-data class Password(val min: Int, val max: Int, val letter: Char, val password: String) {
+data class Password(val min: Int, val max: Int, val letter: Char, val password: String)
 
-}
+
 
